04518d508c93a63a65ca06480b99268d88fc3ca8
YaroslavGrushko <Grushko.kpi@gmail.com>
2019-07-30 10:05:14 +0300

    design from YK

---
+++ public/js/sidebar.js
@@ -0,0 +1,64 @@
+// sidebar
+$('.serviceButton').click(function(){
+    $('html, body').animate({
+        scrollTop: $("#service").offset().top
+    }, 2000);
+
+    $('.serviceButton').addClass('w3-text-teal');
+    $('.aboutButton').removeClass('w3-text-teal');
+    $('.contactButton').removeClass('w3-text-teal');
+    $('.portfolioButton').removeClass('w3-text-teal');
+})
+
+$('.productsButton').click(function(){
+    $('html, body').animate({
+        scrollTop: $("#products").offset().top
+    }, 2000);
+
+    $('.aboutButton').removeClass('w3-text-teal');
+    $('.contactButton').removeClass('w3-text-teal');
+    $('.serviceButton').removeClass('w3-text-teal');
+    $('.productsButton').addClass('w3-text-teal');
+})
+
+$('.aboutButton').click(function(){
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+
+    $('.aboutButton').addClass('w3-text-teal');
+    $('.contactButton').removeClass('w3-text-teal');
+    $('.portfolioButton').removeClass('w3-text-teal');
+    $('.serviceButton').removeClass('w3-text-teal');
+
+})
+
+$('.contactButton').click(function(){
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+
+    $('.aboutButton').removeClass('w3-text-teal');
+    $('.contactButton').addClass('w3-text-teal');
+    $('.portfolioButton').removeClass('w3-text-teal');
+    $('.serviceButton').removeClass('w3-text-teal');
+})
+
+
+
+ // Script to open and close sidebar
+ function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    // document.getElementById("myOverlay").style.display = "block";
+    document.getElementsByClassName("w3-main")[0].style.marginLeft = "300px";
+    $('.open-hamburger').hide();
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+    document.getElementsByClassName("w3-main")[0].style.marginLeft = "0px";
+    // show hamburger button
+    $('.open-hamburger').show();
+
+}
