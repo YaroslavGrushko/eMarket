1705ff458418e1d21f7777865c443ed6e10ade0f
dumerion <dumerion@gmail.com>
2019-07-30 17:39:47 +0300

    correct some names

--- src/App.js
+++ src/App.js
@@ -13,31 +13,6 @@ import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
 
 //webcames array with city name and src to youtube live
 const PLACES={};
-PLACES["Европа"]=[
-  {city:"Saint-Malo-Le Port", src:"https://www.youtube.com/embed/fQ8pFCrVGzE", zip: "Saint-Malo", time_zone:"Europe/Berlin"},
-  {city:"Baden-Baden", src:"https://www.youtube.com/embed/KiKuzd-ioRw", zip: "Baden-Baden", time_zone:"Europe/Berlin"},
-  //{city:"Venice", src:"https://www.youtube.com/embed/YiiNSrDuECw", zip: "Venezia", time_zone:"Europe/Madrid"}
-  {city:"Venice", src:"https://www.youtube.com/embed/vPbQcM4k1Ys", zip: "Moscow", time_zone:"Europe/Madrid"},
-  {city:"Oslo", src:"https://www.youtube.com/embed/DhPYnvZmFQA", zip: "Oslo", time_zone:"Europe/Madrid"}
-];
-PLACES["Азия"]=[
-  {city:"Koh Samui", src:"https://www.youtube.com/embed/y5hjoAZGf_E", zip: "Ko Samui", time_zone:"Asia/Saigon" },
-  {city:"Tokyo", src:"https://www.youtube.com/embed/JYBpu1OyP0c", zip: "Tokyo", time_zone:"Asia/Tokyo"},
-  {city:"Tokyo", src:"https://www.youtube.com/embed/nKMuBisZsZI", zip: "Tokyo", time_zone:"Asia/Tokyo"},
-  {city:"Earth", src:"https://www.youtube.com/embed/qyEzsAy4qeU", zip: "Kiev", time_zone:"Europe/Kiev"}
-];
-PLACES["Америка"]=[
-  {city:"New York", src:"https://www.youtube.com/embed/la90mA4VLa4", zip: "New York", time_zone:"America/New_York"},
-  {city:"Banff", src:"https://www.youtube.com/embed/2UX83tXoZoU", zip: "Banff", time_zone:"Canada/Central"},
-  {city:"Tucson", src:"https://www.youtube.com/embed/nmoQp7gyzIk", zip: "Tucson", time_zone:"America/Fort_Nelson"},
-  {city:"Mexico City", src:"https://www.youtube.com/embed/jHD8XrAYAyk", zip: "Mexico City", time_zone:"America/Mexico_City"}
-];
-PLACES["Африка"]=[
-  {city:"Cape Town", src:"https://www.youtube.com/embed/Ki-d5f5_WwU", zip: "Cape Town", time_zone:"Africa/Cairo"},
-  {city:"Melbourne", src:"https://www.youtube.com/embed/FZ72I6o6Z9k", zip: "Melbourne", time_zone:"Australia/Melbourne"},
-  {city:"Animals", src:"https://www.youtube.com/embed/TW19E-C8nJ8", zip: "Cape Town", zip: "Cape Town"},
-  {city:"Animals", src:"https://www.youtube.com/embed/Kay9czw22ew", zip: "Cape Town", zip: "Cape Town"}
-];
 
 PLACES["Зошити"]=[
   {name:"Зошит1", src:"images/copybooks/cb1.jpg"},
@@ -57,7 +32,6 @@ class Product extends Component{
   }
 }
 
-
 //component of single video:
 class AProduct extends Component{
   render(){
@@ -71,15 +45,15 @@ class AProduct extends Component{
 
 
 //component of several AwebCam components:
-class Section extends Component{
+class Category extends Component{
   render(){
-  const section=this.props.section;
-  const curSECTION=PLACES[section];
-  const Placeslength=curSECTION.length;
+  const category=this.props.category;
+  const curCATEGORY=PLACES[category];
+  const Placeslength=curCATEGORY.length;
     return(
       <Container>
       <Row md={6} sm={6}>
-      {curSECTION.map((product, index) => (
+      {curCATEGORY.map((product, index) => (
       <Col key={index} md={6} sm={6}>
         <table>
           <tr>
@@ -104,7 +78,7 @@ class BackButton extends Component{
     return(
       <div>
         <button className="BackButton button_back" onClick={()=>this.props.onClick()}>
-          <span className="CenturyGothicCommon">go <b>back</b> to the countries</span>
+          <span className="CenturyGothicCommon">go <b>back</b> to the categories</span>
         </button>
       </div>
     );
@@ -117,11 +91,11 @@ class App extends Component {
     super(props);
     this.state = {
       showProduct:false,
-      section:"Зошити",
+      category:"Зошити",
       product:"none"
     };
   }
-//Country button handler function:
+//Product button handler function:
 handleClick(productName) {
     this.setState({
       product:productName,
@@ -131,13 +105,13 @@ handleClick(productName) {
 //"to back" button handler function:
 handleBackClick(){
   this.setState({
-    showProduct:!this.state.showProduct,
+    showProduct:false,
   });
 }
 //function that returns <Countries/> tag(component):
-renderProducts(){
+renderCategory(){
   return(
-    <Section key={0} section={this.state.section} onClick={(i) => this.handleClick(i)} />
+    <Category category={this.state.category} onClick={(productName) => this.handleClick(productName)} />
     );
 }
 //function that returns <BackButton/> and <WebCames/> tag(component):
@@ -153,7 +127,7 @@ let products=[];
     return (
       <div className="App">
         
-      {this.state.showProduct ? this.renderProduct() : this.renderProducts()}
+      {this.state.showProduct ? this.renderProduct() : this.renderCategory()}
 
       </div>
     );
