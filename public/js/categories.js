327fc745831b5afc827568e584ac9518a900430a
dumerion <dumerion@gmail.com>
2019-08-07 18:46:11 +0300

    write new category to db

--- public/js/categories.js
+++ public/js/categories.js
@@ -7,17 +7,27 @@ if(selectedId=='addCategoryButton')uploadOrders()
 })
 // upload all orders from buyers
 function uploadOrders(){
+  let jsonData = {
+    'manager_id' : 7,
+    'manager_name' : "Ярослав",
+    'category_name' : "Степлери",//$('#').val(),
+    'category_code': "fa-fa-fa"
+  };
+  let Data_order = JSON.stringify(jsonData);
 
-    $.ajax({
-      url:'http://127.0.0.1:5000/tracks',
-      data:' ',
-      type:'GET',
-      success:function(jsonData){
-        var data = $.parseJSON(jsonData);
-        alert(data)
-      },
-      error:function(error) {
-        alert( "error: " +JSON.stringify(error));
-      }
-    });
-  }
+  $.ajax({
+    url: 'http://127.0.0.1:5000/categories',
+    type: 'POST',
+    headers: {'Content-Type' : 'application/json'},
+    data: Data_order,
+    success: function(data) { 
+      alert(data.status); 
+    },
+    error:function(error) {
+            alert( "error: " +JSON.stringify(error));
+          }
+});
+
+}
+
+  
