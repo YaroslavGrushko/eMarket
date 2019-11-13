73beb7ff5afe93e0d2e408e0ccc7074c40ac25f0
YaroslavGrushko <Grushko.kpi@gmail.com>
2019-11-13 18:03:25 +0200

    commit2

--- src/dev_Yaroslav.js
+++ src/dev_Yaroslav.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import {InputGroup} from 'react-bootstrap';
 import {FormControl} from 'react-bootstrap';
+
+import "bootstrap/dist/css/bootstrap.css"; //подключаем только грид
+import { Container, Row, Col } from "react-bootstrap";
+
 import Chart from 'chart.js';
 import './css/my-charts.css';
 
@@ -30,7 +34,7 @@ import './css/my-charts.css';
 // }
 
 // component Total Expenses:
-class TotalExpenses extends Component {
+class TotalSales extends Component {
 //   componentDidMount() {
 //     this.initializeChart(this.props.config);
 // }
@@ -68,18 +72,24 @@ initializeChart =()=> {
   render(){
     return(
       <div className="d-flex justify-content-center">
-        <div className="card" style={{width:40+'%'}}>
-            <div className="card-title d-flex justify-content-center">
-                <div className="d-flex flex-row">
-                    <img src={this.props.photo}></img>    
-                    <h4>Продажі відділу "{this.props.category_name}"</h4>
-                    <span>менеджер {this.props.name}</span>
-                    
-                    <div className="card text-center font-weight-bold ml-2 p-1">3500</div>
-                    <div className="ml-1">грн.</div>
-                </div>
+        <div className="card mb-2">
+            <div className="card-title text-center">                                   
+                    <h4>Продажі відділу "{this.props.category_name}"</h4>                                         
             </div>
-            <div className="card-body p-0 d-flex justify-content-center">
+            <div className="card-body p-0 d-flex justify-content-center flex-column">
+
+                  <div className="d-flex flex-row">
+                    <img src={this.props.photo}  style={{height:80+'px'}} className="m-2"></img>
+                      <div className="mt-4">
+                        <span className="text-center">менеджер {this.props.name}</span>
+                        <div className="d-flex flex-row text-center">
+                          <span>всього:</span>
+                          <div className="card text-center font-weight-bold ml-2 p-1">3500</div>
+                          <div className="ml-1">грн.</div>
+                        </div>
+                      </div> 
+                  </div>
+                
                 <div className="chart-container">
                     <canvas id={this.props.chartId} ref={this.initializeChart} aria-label="Hello ARIA World" role="img"  />
                 </div>
@@ -125,9 +135,11 @@ componentDidMount()  {
       chartData.push([100, 200, 400, 500, 900]);
 
       var categories = []
+      
       for(let i=0; i<category_names.length;i++){
-        categories.push(<TotalExpenses chartId={"chart"+i} receivedData={chartData[i]} category_name={category_names[i]} name={names[i]} photo={photos[i]}/>);
+        categories.push(<Col xs={12} md={6}><TotalSales chartId={"chart"+i} receivedData={chartData[i]} category_name={category_names[i]} name={names[i]} photo={photos[i]}/></Col>);
       }
+    
       this.setState({
         categories: categories,
       })
@@ -136,7 +148,11 @@ componentDidMount()  {
   render(){
     return(
     <div>
+    <Container>
+    <Row>
     {this.state.categories}
+    </Row>
+    </Container>
     </div>
     );
   }
