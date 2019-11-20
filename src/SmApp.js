1d139cdcb797c04009db07e3beb7897844c6596f
YaroslavGrushko <Grushko.kpi@gmail.com>
2019-11-20 10:31:00 +0200

    sm-header

--- src/SmApp.js
+++ src/SmApp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './css/main_App.css';
-
+import './css/images.css';
 
 import "bootstrap/dist/css/bootstrap.css"; //подключаем только грид
 import { Container, Row, Col } from "react-bootstrap";
@@ -10,7 +10,13 @@ import { Container, Row, Col } from "react-bootstrap";
 class SmApp extends Component {
     ////////////////////////////////////////////
   render() {
-      return("Hello from SmApp");
+      return(<div className='d-flex flex-row'>
+              <img className='avatar_img mr-4' src='images/manager-flat.jpg' alt='менеджер' />
+              <div>
+                <h2>Ярослав Грушко</h2>
+                <h3>менеджер-продавець</h3>
+              </div>
+            </div>);
   }
     ////////////////////////////////////////////
 }
